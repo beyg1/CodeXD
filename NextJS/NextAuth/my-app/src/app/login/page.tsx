f39a166fbc19c2f9722c196d77a614e212e44403
beyg1 <abdulrubbaig1@gmail.com>
2024-12-26 15:45:51 +0500

    nextAuth container app

---
+++ NextJS/NextAuth/my-app/src/app/login/page.tsx
@@ -0,0 +1,19 @@
+import Header from '@/components/Header'
+import React from 'react'
+
+export default function login() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+        <Header/>        
+        <main className="pt-20 pb-16">
+        <div className="max-w-7xl mx-auto">
+          <div className="text-center mb-12">
+            <h1 className="text-4xl font-bold text-gray-900 mb-4">
+              Login
+            </h1>        
+          </div>
+        </div>
+      </main>
+    </div>
+  )
+}
--- NextJS/NextAuth/my-app/src/components/Header.tsx
+++ NextJS/NextAuth/my-app/src/components/Header.tsx
@@ -3,13 +3,12 @@ import Link from 'next/link';
 
 const Header: React.FC = () => {
   return (
-    <header className="bg-zinc-400 border-b shadow-md rounded-full">
+    <header className="bg-slate-300 border-b shadow-md rounded-full">
       <nav className="container mx-auto flex justify-between items-center px-4">
         <div className="flex space-x-2 m-4">
           <Link
             href="/"
-            className="text-gray-800 font-bold hover:bg-gray-700 rounded-full border "
-            style={{ backgroundColor: '#f3f4f6', padding: '1.5rem' }}
+            className="font-bold rounded-full border transition-colors duration-200 bg-gray-100 hover:bg-orange-300 px-4 py-4"
           >
             Home
           </Link>
@@ -17,15 +16,13 @@ const Header: React.FC = () => {
         <div className="flex space-x-2 mx-4">
           <Link
             href="/dashboard"
-            className="text-gray-800 hover:bg-gray-300 rounded-full border "
-            style={{ backgroundColor: '#f3f4f6', padding: '1.5rem' }}
+            className="rounded-full border transition-colors duration-200 bg-gray-100 hover:bg-blue-300 px-4 py-4"
           >
             Dashboard
           </Link>
           <Link
             href="/login"
-            className="text-gray-800 hover:bg-gray-300 rounded-full border  "
-            style={{ backgroundColor: '#f3f4f6', padding: '1.5rem' }}
+            className="rounded-full border transition-colors duration-200 bg-gray-100 hover:bg-teal-300 px-4 py-4"
           >
             Login
           </Link>
