b03320ccf9cee3fbee391a95f800d1bcad726d0c
beyg1 <abdulrubbaig1@gmail.com>
2025-01-30 19:19:05 +0500

    prototype2

--- NextJS/Prototype/prototype2/src/app/page.tsx
+++ NextJS/Prototype/prototype2/src/app/page.tsx
@@ -4,10 +4,12 @@ import Services from "@/components/sections/Services";
 import HowItWorks from "@/components/sections/HowItWorks";
 import Testimonials from "@/components/sections/Testimonials";
 import Footer from "@/components/sections/Footer";
+import AIStatusIndicator from "@/components/ui/AiStatus";
 
 export default function Home() {
   return (
     <>
+      <AIStatusIndicator />
       <Hero />
       <Features />
       <Services />
