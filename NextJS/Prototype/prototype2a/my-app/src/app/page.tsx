e1efce071ce7607133d583625447771a6f963a34
beyg1 <abdulrubbaig1@gmail.com>
2025-02-04 04:59:10 +0500

    ok

--- NextJS/Prototype/prototype2a/my-app/src/app/page.tsx
+++ NextJS/Prototype/prototype2a/my-app/src/app/page.tsx
@@ -3,7 +3,7 @@ import Hero from "@/components/sections/hero";
 
 export default function Home() {
   return (
-    <section className="relative h-screen flex items-center justify-center overflow-hidden ">
+    <section className="bg-gradient-to-r from-[#010012] to-[#1b2e55] ">
       <DottedBackground />
       <Hero />      
       
