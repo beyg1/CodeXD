54a740cfd21cdac26c6d66a19f09529e81ae0ac2
beyg1 <abdulrubbaig1@gmail.com>
2025-02-10 03:47:00 +0500

    ok

--- NextJS/Prototype/prototype2a/my-app/src/components/sections/Navbar.tsx
+++ NextJS/Prototype/prototype2a/my-app/src/components/sections/Navbar.tsx
@@ -5,8 +5,8 @@ import { Store, User, Settings, HelpCircle,  } from "lucide-react"
 const CylindricalNavbar: React.FC = () => {
   return (
     <nav className="fixed top-4 left-36 right-36 z-50 p-4 flex justify-between items-center w-auto rounded-full bg-gradient-to-r from-white/10 to-white/30 backdrop-blur-sm backdrop-hue-rotate-0  shadow-2xl">
-      <Button variant="ghost" className="text-white/90 hover:text-white hover:bg-white/5 transition-colors">
-        <Store className="mr-2 h-4 w-4" /> Agentia World
+      <Button variant="ghost" className="text-white/90  hover:bg-white/5 transition-colors">
+        <Store className="mr-1" /> Agentia World
       </Button>
       <div className="flex space-x-2">
         {[User, Settings, HelpCircle].map((Icon, index) => (
