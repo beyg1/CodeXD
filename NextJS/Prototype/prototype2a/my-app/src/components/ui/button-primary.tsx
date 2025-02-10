3138d8caeefb40ea1d8dd1bd24478deb5c1b0782
beyg1 <abdulrubbaig1@gmail.com>
2025-02-10 23:40:38 +0500

    button fix

--- NextJS/Prototype/prototype2a/my-app/src/components/ui/button-primary.tsx
+++ NextJS/Prototype/prototype2a/my-app/src/components/ui/button-primary.tsx
@@ -9,12 +9,12 @@ interface ButtonPrimaryProps {
 
 export default function ButtonPrimary({ children, className, href, target }: PropsWithChildren<ButtonPrimaryProps>) {
     return (
-        <a 
-          href={href}
-          target={target}
-          className={`inline-block text-lg text-white py-4 px-8 rounded-full bg-gradient-to-r from-white/10 to-white/30 backdrop-blur-sm backdrop-hue-rotate-0 shadow-2xl hover:animate-pulse hover:font-bold hover:bg-transparent hover:border-white hover:border-2 ${className || ""}`}>
-            {children}
-        </a>
+      <a 
+      href={href}
+      target={target}
+      className={`inline-block text-lg text-white py-4 px-8 rounded-full bg-gradient-to-r from-white/10 to-white/30 backdrop-blur-sm backdrop-hue-rotate-0 shadow-2xl hover:animate-pulse hover:font-bold hover:bg-transparent hover:border-white hover:border-2 ${className || ""}`}>
+        {children}
+    </a>
     )
 }
 
