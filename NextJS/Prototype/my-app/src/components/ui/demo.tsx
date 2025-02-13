bc0dfe19a145d3a37edb29d6711d15b429a87edf
beyg1 <abdulrubbaig1@gmail.com>
2025-02-14 00:12:52 +0500

    ok

--- NextJS/Prototype/my-app/src/components/ui/demo.tsx
+++ NextJS/Prototype/my-app/src/components/ui/demo.tsx
@@ -6,20 +6,19 @@ import { Spotlight } from "@/components/ui/spotlight"
  
 export default function SplineSceneBasic() {
   return (
-    <Card className="w-full h-[500px] bg-transparent relative overflow-hidden">
+    <Card className="w-full h-[500px] bg-transparent border relative overflow-hidden">
       <Spotlight
         className="-top-40 left-0 md:left-60 md:-top-20"
       />
       
-      <div className="flex h-full">        
-        {/* Right content */}
+      
         <div className="flex-1 relative">
           <SplineScene 
             scene="https://prod.spline.design/kZDDjO5HuC9GJUM2/scene.splinecode"
             className="w-full h-full"
           />
         </div>
-      </div>
+      
     </Card>
   )
 }
