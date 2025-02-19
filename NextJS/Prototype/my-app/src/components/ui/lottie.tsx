eb639ad3b112cd2f0959dd2937c9b4bcfd30c2d0
beyg1 <abdulrubbaig1@gmail.com>
2025-02-19 19:16:36 +0500

    ok

--- NextJS/Prototype/my-app/src/components/ui/lottie.tsx
+++ NextJS/Prototype/my-app/src/components/ui/lottie.tsx
@@ -4,12 +4,10 @@ import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
 export default function Lottie() {
   return (
-    <div className="w-full max-w-[500px]">
       <DotLottieReact
         src="Lottie.lottie"
         autoplay
-        loop
-      />
-    </div>
+        loop  
+      />  
   );
 };
