e9e5c71df5323aee42170b032238ee78687cae13
beyg1 <abdulrubbaig1@gmail.com>
2025-02-19 19:08:50 +0500

    ok

--- NextJS/Prototype/my-app/src/components/sections/hero.tsx
+++ NextJS/Prototype/my-app/src/components/sections/hero.tsx
@@ -20,7 +20,7 @@ export default function Hero() {
             <h2 className="mt-8 text-white sm:text-xl md:text-2xl lg:text-3xl font-medium md:pl-8">
               Select opensource AI Agents from our library & transform your business. Request custom AI Agents for your unique Automation workflows from developers, who are contributing to the opensource Eco System.
             </h2>
-            <ButtonPrimary className="mt-12 self-start md:pl-8" href="https://www.youtube.com/watch?v=9PoGWIK9DmE" target="_blank">
+            <ButtonPrimary className="mt-12 mx-8 self-start md:pl-8" href="https://www.youtube.com/watch?v=9PoGWIK9DmE" target="_blank">
               See Demo
             </ButtonPrimary>
           </div>
