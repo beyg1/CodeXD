489ac7d49bb13487b0903f61f331b32f16b76026
beyg1 <abdulrubbaig1@gmail.com>
2025-02-19 23:45:36 +0500

    ok

--- NextJS/Prototype/my-app/src/components/sections/hero.tsx
+++ NextJS/Prototype/my-app/src/components/sections/hero.tsx
@@ -7,11 +7,11 @@ export default function Hero() {
     <section className="relative w-full min-h-screen bg-neutral-800">
       <AnimatedGridPatternDemo />
       <div className="relative z-10 flex min-h-screen px-4 py-8">
-        <div className="container mx-auto flex flex-col md:flex-row-reverse items-center justify-between gap-8">
+        <div className="container mx-auto ml-auto mr-0 flex flex-col items-center justify-end ">
           {/* Lottie Animation */}
-          <div className="w-full md:w-1/2 flex justify-center items-center md:absolute md:top-1/2 md:left-1/2 md:-translate-x-1/5 md:-translate-y-1/2 ">
+          {/* <div className="w-full md:w-1/2 flex justify-center items-center md:absolute md:top-1/2 md:left-1/2 md:-translate-x-1/5 md:-translate-y-1/2 "> */}
             <Lottie />
-          </div>
+          {/* </div> */}
           {/* Text Content */}
           <div className="flex flex-col w-full md:w-1/2 md:absolute md:bottom-0 md:left-0 md:mt-auto md:ml-auto md:mb-auto md:mr-8 order-1 md:order-1">
             <h1 className="text-white text-4xl sm:text-5xl md:text-5xl lg:text-6xl font-bold tracking-tight leading-tight md:pl-8">
@@ -20,7 +20,7 @@ export default function Hero() {
             <h2 className="mt-8 text-white sm:text-xl md:text-2xl lg:text-3xl font-medium md:pl-8">
               Select opensource AI Agents from our library & transform your business. Request custom AI Agents for your unique Automation workflows from developers, who are contributing to the opensource Eco System.
             </h2>
-            <ButtonPrimary className="mt-12 mx-8 self-start md:pl-8" href="https://www.youtube.com/watch?v=9PoGWIK9DmE" target="_blank">
+            <ButtonPrimary className="mt-12 mb-4 mx-8 self-start md:pl-8" href="https://www.youtube.com/watch?v=9PoGWIK9DmE" target="_blank">
               See Demo
             </ButtonPrimary>
           </div>
