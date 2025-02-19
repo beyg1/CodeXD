ffa6dcb83d8250a02d9262f2aafd615c44263855
beyg1 <abdulrubbaig1@gmail.com>
2025-02-19 17:12:31 +0500

    ok

--- NextJS/Prototype/my-app/src/components/sections/hero.tsx
+++ NextJS/Prototype/my-app/src/components/sections/hero.tsx
@@ -1,27 +1,21 @@
-import  ButtonPrimary  from "../ui/button-primary";
-import DiagonalGridBackground from "../ui/grid-bg";
+import ButtonPrimary from "../ui/button-primary";
+import AnimatedGridPatternDemo from "../ui/magic-grid";
 
 export default function Hero() {
   return (
-    <section className="relative overflow-hidden min-h-screen flex flex-col items-center justify-center md:items-start md:text-left">
-        <DiagonalGridBackground />
-        <div className="absolute inset-0 flex flex-col items-center justify-center px-4 md:items-start md:text-left">
-      
+    <section className="relative w-full min-h-screen">
+      <AnimatedGridPatternDemo />
+      <div className="relative z-10 flex flex-col items-center justify-center min-h-screen px-4 md:items-start md:text-left">
         <h1 className="text-white text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold tracking-tight leading-tight">
           Introduce Reasoning & LLMs to your <span className="text-red-600">Automated WorkFlow</span>
         </h1>
         <h2 className="mt-8 text-white sm:text-xl md:text-2xl lg:text-3xl font-medium max-w-2xl">
           Select opensource AI Agents from our library & transform your business. Request custom AI Agents for your unique Automation workflows from developers, who are contributing to the opensource Eco System.
-        </h2>        
+        </h2>
         <ButtonPrimary className="mt-12" href="https://www.youtube.com/watch?v=9PoGWIK9DmE" target="_blank">
           See Demo
-        </ButtonPrimary>  
-        </div>
-        
-        {/* <div className="absolute top-48 right-20 w-1/2 h-1/2">  
-            place for an interactive element         
-        </div>             */}
+        </ButtonPrimary>
+      </div>
     </section>
-   
   );
 }
---
+++ NextJS/Prototype/my-app/src/components/ui/lottie.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
+
+const App = () => {
+  return (
+    <DotLottieReact
+      src="https://lottie.host/0b580331-8ab0-422c-a6ca-636be11619bc/V9lbFGSKc7.lottie"
+      loop
+      autoplay
+    />
+  );
+};
