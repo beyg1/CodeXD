82298c2bf36d5f1baf7783e59fe68dded68d86ce
beyg1 <abdulrubbaig1@gmail.com>
2025-02-19 18:41:55 +0500

    ok

--- NextJS/Prototype/my-app/src/components/sections/hero.tsx
+++ NextJS/Prototype/my-app/src/components/sections/hero.tsx
@@ -7,9 +7,13 @@ export default function Hero() {
     <section className="relative w-full min-h-screen bg-neutral-800">
       <AnimatedGridPatternDemo />
       <div className="relative z-10 flex min-h-screen px-4 py-8">
-        <div className="container mx-auto flex flex-col md:flex-row items-center justify-between gap-8">
+        <div className="container mx-auto flex flex-col md:flex-row-reverse items-center justify-between gap-8">
+          {/* Lottie Animation */}
+          <div className="w-full md:w-1/2 flex justify-center items-center md:absolute md:top-0 md:right-0">
+          <Lottie />
+          </div>
           {/* Text Content */}
-          <div className="flex flex-col w-full md:w-1/2">
+          <div className="flex flex-col w-full md:w-1/2 md:absolute md:bottom-0 md:left-0 md:mt-auto md:ml-auto md:mb-auto md:mr-8 md:mb-8">
             <h1 className="text-white text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold tracking-tight leading-tight">
               Introduce Reasoning & LLMs to your <span className="text-red-600">Automated WorkFlow</span>
             </h1>
@@ -19,11 +23,6 @@ export default function Hero() {
             <ButtonPrimary className="mt-12 self-start" href="https://www.youtube.com/watch?v=9PoGWIK9DmE" target="_blank">
               See Demo
             </ButtonPrimary>
-          </div>
-          
-          {/* Lottie Animation */}
-          <div className="w-full md:w-1/2 flex justify-center items-center">
-            <Lottie />
           </div>
         </div>
       </div>
