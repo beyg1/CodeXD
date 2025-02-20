33aa570693df17bc29acc752fdf79c79d575868b
beyg1 <abdulrubbaig1@gmail.com>
2025-02-20 12:32:43 +0500

    ok

--- NextJS/Prototype/my-app/src/components/sections/hero.tsx
+++ NextJS/Prototype/my-app/src/components/sections/hero.tsx
@@ -16,7 +16,7 @@ export default function Hero() {
           <div className="flex flex-col w-full md:w-1/2 md:absolute md:bottom-0 md:left-0 md:mt-auto md:ml-auto md:mb-auto md:mr-8 order-1 md:order-1">
             <h1 className="text-white text-4xl sm:text-5xl md:text-4xl lg:text-5xl font-bold tracking-tight leading-tight md:pl-8">
             AI Agents – Discover, Buy & 
-            <span className="text-red-600">Build the Future.</span>
+            <span className="text-red-600"> Build the Future.</span>
             </h1>
             <h2 className="mt-8 text-white sm:text-xl md:text-2xl lg:text-2xl font-medium md:pl-8">
             Introduce Reasoning & Add Artificial Intelligence to your Automated Work Flows. Select opensource AI Agents from our library or simply request custom AI Agents for your unique Automation Work Flow.
